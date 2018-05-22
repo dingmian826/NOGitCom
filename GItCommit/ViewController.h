bcd21ed0f56927943605d43231bd9f64dd264232
dingmian <dingmian@ayibang.com>
2018-05-22 16:22:53 +0800

    如果创建项目时候没有选择git，就需要使用终端帮助创建git

---
+++ GItCommit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GItCommit
+//
+//  Created by ayb on 2018/5/22.
+//  Copyright © 2018年 testDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
